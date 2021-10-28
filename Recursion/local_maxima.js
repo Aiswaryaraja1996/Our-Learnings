c2fb2fb3766700b7c48e3bfa1ebc8ec453d01236
Antony-Anto <antonychalissery10@gmail.com>
2021-10-29 00:16:02 +0530

    Updated code

--- Recursion/local_maxima.js
+++ Recursion/local_maxima.js
@@ -10,26 +10,46 @@ function runProgram(n) {
     }
 }
 function localMaxima(counts, values, localMax) {
-    
     if (values.length < 3) {
         console.log("-1");
     }
-    else if (counts < values.length) {
-        
-        if (values[counts] > values[counts - 1] && values[counts] > values[counts + 1]) {
-            //console.log(values[counts]);
-            localMax++;
+
+    else{
+        for(var i=counts;i<values.length-1;i++){
+            if(values[i] > values[i - 1] && values[i] > values[i + 1]){
+               // console.log(values[i]);
+                localMax++;
+            }
         }
-        counts++
-        localMaxima(counts, values, localMax);
-    }
-    else {
         if (localMax > 0) console.log(localMax);
         else console.log("-1");
     }
+    
+    // if (values.length < 3) {
+    //     console.log("-1");
+    // }
+    // else if (counts < (values.length-1)) {
+        
+    //     if (values[counts] > values[counts - 1] && values[counts] > values[counts + 1]) {
+    //         //console.log(values[counts]);
+    //         localMax++;
+    //     }
+    //     counts++
+    //     localMaxima(counts, values, localMax);
+    // }
+    // else {
+    //     if (localMax > 0) console.log(localMax);
+    //     else console.log("-1");
+    // }
 }
-runProgram(`2
-2
-2 2
+runProgram(`5
+1
+1
 3
-2 2 1`);
+1 2 1
+2
+2 1
+5
+2 4 3 5 1
+6
+1 2 3 4 5 6`);
